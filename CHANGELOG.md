96111ec664d7d7e2715b906c7a28d9a6f8bd538b
Terrence Foulk <terrence@itwaisi.com>
2023-08-30 21:11:42 -0500

    create html to pdf function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,3 +20,14 @@
 - refactor function to convert pdf to docx
 - refactor function to convert docx to html
 - add image converter function to docx to function
+
+## 2023-08-29
+
+- create `/examples` folder
+
+## 2023-08-30
+
+- add `/output` folder creation to `pdfweb.__init__`
+- update `README.md`
+- add `/output` folder paths to `pdfweb.defaults`
+- create hmtl to pdf function in `pdfweb.webtopdf`
--- pdfweb/defaults.py
+++ pdfweb/defaults.py
@@ -1,3 +1,12 @@
 DEFAULTS = {
-    'program': 'PDFWEB'
+    'program': 'PDFWEB',
+    'folders': {
+        'output_base': './output',
+        'output_pdftoweb': './output/pdftoweb',
+        'output_pdftoweb_temp': './output/pdftoweb/temp',
+        'output_pdftoweb_final': './output/pdftoweb/final',
+        'output_webtopdf': './output/webtopdf',
+        'output_webtopdf_temp': './output/webtopdf/temp',
+        'output_webtopdf_final': './output/webtopdf/final',
+    }
 }
